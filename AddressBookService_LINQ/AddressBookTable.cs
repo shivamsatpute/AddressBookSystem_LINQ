c24feae2cb41dfc569e40b3ddfea59ce764a4aa7
shivam satpute <shivam897satpute@gmail.com>
2021-06-17 00:56:47 +0530

    [shivam]add: Adding Attributes

--- AddressBookService_LINQ/AddressBookTable.cs
+++ AddressBookService_LINQ/AddressBookTable.cs
@@ -1,11 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Text;
 
 namespace AddressBookService_LINQ
 {
     class AddressBookTable
     {
-      
+      public void CreateAddressBookDataTable()
+        {
+            DataTable table = new DataTable();
+            table.Columns.Add("First Name", typeof(string));
+            table.Columns.Add("Last Name", typeof(string));
+            table.Columns.Add("Address", typeof(string));
+            table.Columns.Add("City", typeof(string));
+            table.Columns.Add("State", typeof(string));
+            table.Columns.Add("Zip", typeof(int));
+            table.Columns.Add("PhoneNumber", typeof(long));
+            table.Columns.Add("Email", typeof(string));
+        }
     }
 }
