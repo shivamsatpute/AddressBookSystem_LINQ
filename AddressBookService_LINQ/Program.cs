711ca82234e0f3c824db858028e9ba928a597ecd
shivam satpute <shivam897satpute@gmail.com>
2021-06-17 00:42:04 +0530

    [shivam]add: Create Data Table

---
+++ AddressBookService_LINQ/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AddressBookService_LINQ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Address Book System");
+        }
+    }
+}
